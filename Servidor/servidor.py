7ee8c9230f8bd2210a6b71692a8ea97b46900c68
psalazar211 <104046146+psalazar211@users.noreply.github.com>
2022-11-05 10:54:53 -0600

    add audios

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,9 +1,10 @@
 from detectorDTMF import get_tone
-import speech_recognition as sr
+from playsound import playsound
 
 password = '1234'
 
 def wait():
+    playsound("audios/wait.wav")
     print("Marque cualquier tecla para comenzar")
     number = get_tone()
     print(number)
@@ -14,15 +15,21 @@ def wait():
 
 def ini():
     print("Bienvenido")
+    playsound("audios/ini.wav")
 
 def getPasssword (password):
+    playsound("audios/getPassword.wav")
     print("Digite la contrasena")
+    playsound("audios/primero.wav")
     print("Ingrese primer digito de la contrasena")
     number1 = get_tone()
+    playsound("audios/segundo.wav")
     print("Ingrese segundo digito de la contrasena")
     number2 = get_tone()
+    playsound("audios/tercero.wav")
     print("Ingrese tercer digito de la contrasena")
     number3 = get_tone()
+    playsound("audios/cuarto.wav")
     print("Ingrese cuarto digito de la contrasena")
     number4 = get_tone()
     listenPassword = number1 + number2 + number3 + number4
@@ -37,7 +44,7 @@ def getPasssword (password):
 
 def readMessages():
     print("No hay mensajes nuevos")
-
+    
 def printNumber(number):
     if number == '1':
         print('1')
@@ -71,13 +78,18 @@ def printNumber(number):
         print('.')
 
 def changePassword():
+    playsound("audios/changePassword.wav")
     print("Digite la nueva contrasena")
+    playsound("audios/primero.wav")
     print("Ingrese primer digito de la nueva contrasena")
     number1 = get_tone()
+    playsound("audios/segundo.wav")
     print("Ingrese segundo digito de la nueva contrasena")
     number2 = get_tone()
+    playsound("audios/tercero.wav")
     print("Ingrese tercer digito de la nueva contrasena")
     number3 = get_tone()
+    playsound("audios/cuarto.wav")
     print("Ingrese cuarto digito de la nueva contrasena")
     number4 = get_tone()
     password = number1 + number2 + number3 + number4
@@ -90,7 +102,8 @@ def changePassword():
     return password
 
 def options():
-    print("Digite 1 para leer sus mensajes o 2 para camabiar la contrasena")
+    playsound("audios/options.wav")
+    print("Digite 1 para leer sus mensajes o 2 para cambiar la contrasena")
     number = get_tone()
     if number == '1':
         readMessages()
